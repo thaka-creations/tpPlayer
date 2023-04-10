98640401725186c601647c1c795966db6636389c
abdulnelfrank <nelfrankaj@gmail.com>
2023-04-10 17:29:55 +0300

    player updates

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -1,8 +1,10 @@
 import base64
+from datetime import datetime
+from threading import Thread
 
-from PyQt6.QtCore import QSettings
-from PyQt6.QtNetwork import QNetworkAccessManager
-from PyQt6.QtWidgets import QDialog
+from PyQt6.QtCore import QSettings, QUrl
+from PyQt6.QtNetwork import QNetworkAccessManager, QNetworkRequest, QNetworkReply
+from PyQt6.QtWidgets import QDialog, QMessageBox
 
 from controllers.login_controller import LoginController
 from ui_files.main_ui import Ui_MainWindow
@@ -28,9 +30,44 @@ class MainController(Ui_MainWindow):
         self.activateKeyButton.clicked.connect(self.activate_key)
         self.accountButton.clicked.connect(self.navigate_to_account)
 
+        # account page
+        self.logoutButton.clicked.connect(self.logout)
+        self.backButton.clicked.connect(self.navigate_to_home)
+        self.viewKeyButton.clicked.connect(self.navigate_to_local_keys)
+        self.registeredKeyButton.clicked.connect(self.fetch_online_keys)
+
+        # keys page
+        self.backButton2.clicked.connect(self.navigate_to_account)
+
     # open player button
     def open_player(self):
         self.stackedWidget.setCurrentIndex(1)
+
+    # logout user
+    def logout(self):
+        utils.delete_headers()
+        self.stackedWidget.setCurrentIndex(0)
+        self.MainWindow.close()
+        self.redirect_to_login()
+
+    def navigate_to_home(self):
+        self.stackedWidget.setCurrentIndex(0)
+
+    def navigate_to_local_keys(self):
+        self.keyListWidget.clear()
+        keys = utils.get_local_keys()
+        try:
+            for key in keys:
+                self.keyListWidget.addItem(key['key'])
+        except Exception as e:
+            print(e)
+            self.display_message("No keys found")
+        self.titleLabel3.setText("REGISTERED KEYS")
+        self.stackedWidget.setCurrentIndex(3)
+
+    # fetch online keys
+    def fetch_online_keys(self):
+        self.check_internet_connection(keys=True)
 
     # activate key button
     def activate_key(self):
@@ -56,11 +93,82 @@ class MainController(Ui_MainWindow):
 
         self.stackedWidget.setCurrentIndex(2)
 
-    def check_internet_connection(self):
-        pass
+    # check internet connection
+    def check_internet_connection(self, keys=False):
+        if keys:
+            self.nam.finished.connect(self.on_result)
+        else:
+            self.nam.finished.connect(self.on_alt_result)
+        self.nam.get(QNetworkRequest(QUrl("https://www.google.com")))
+
+    # internet response handler
+    def on_result(self, reply: QNetworkReply):
+        if not reply.error():
+            self.display_message("Error", "This action requires internet connection")
+            return
+        status_code, message = utils.get_registered_keys()
+        if not status_code:
+            self.display_message("Error", message)
+            return
+        Thread(target=utils.sync_keys, daemon=False).start()
+        self.keyListWidget.clear()
+        try:
+            for key in message:
+                self.keyListWidget.addItem(key)
+            self.stackedWidget.setCurrentIndex(3)
+            self.titleLabel3.setText("REGISTERED KEYS ON OTHER DEVICES")
+            del message
+
+            # check for new version
+            status, message = utils.check_for_new_version()
+            if not status:
+                self.display_message("Error", message)
+                return
+        except Exception as e:
+            print(e)
+            self.display_message("No keys found")
+
+    def on_alt_result(self, reply: QNetworkReply):
+        if not reply.error():
+            reset = False
+            next_connection_date = utils.get_next_connection_date()
+            if not next_connection_date:
+                reset = True
+            else:
+                if datetime.now().date() > datetime.strptime(next_connection_date, "%Y-%m-%d").date():
+                    reset = True
+            if reset:
+                utils.delete_headers()
+                self.stackedWidget.setCurrentIndex(0)
+                self.MainWindow.close()
+                self.redirect_to_login()
+        else:
+            utils.set_next_connection_date()
+            Thread(target=utils.sync_keys, daemon=False).start()
+
+            # check for new version
+            status, message = utils.check_for_new_version()
+            if not status:
+                self.display_message("Error", message)
+                return
 
     def redirect_to_login(self):
         dialog = QDialog()
-        login_dialog = LoginController()
+        login_dialog = LoginController(self.MainWindow)
         login_dialog.setupUi(dialog)
         dialog.exec()
+
+    # display message box
+    @staticmethod
+    def display_message(status_code, message):
+        message_box = QMessageBox()
+        message_box.setWindowTitle(status_code)
+        message_box.setText(message)
+        message_box.setStandardButtons(QMessageBox.StandardButton.Ok)
+
+        if status_code == "Success":
+            message_box.setIcon(QMessageBox.Icon.Information)
+        else:
+            message_box.setIcon(QMessageBox.Icon.Warning)
+
+        message_box.exec()
--- utils.py
+++ utils.py
@@ -1,11 +1,21 @@
+import base64
+import hashlib
+import json
+import os
+import platform
+import plistlib
+import subprocess
 import sys
 from base64 import b64encode
+from datetime import datetime, timedelta
+from threading import Thread
 
 import requests
+from cryptography.fernet import Fernet
 from PyQt6.QtCore import QSettings
 from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QMessageBox
 
+import settings
 from settings import BASE_URL
 
 ss = QSettings("TafaPlayer", "TafaPlayer")
@@ -102,3 +112,223 @@ def verify_otp(payload):
             return False, response.json()['message']
     except requests.exceptions.ConnectionError:
         return False, "Could not connect to server. Make sure you are connected to the internet"
+
+
+# get local storage keys, read keys from secure storage
+def get_local_keys():
+    try:
+        keys = ss.value('keys')
+        if not isinstance(keys, list):
+            return [keys]
+        return keys
+    except Exception as e:
+        print(e)
+        return []
+
+
+# get last connected date
+def get_next_connection_date():
+    if ss.value('last_date_connected'):
+        return ss.value('last_date_connected')
+    return None
+
+
+# set next connection date
+def set_next_connection_date():
+    next_date = datetime.now() + timedelta(days=30)
+    ss.setValue("last_date_connected", next_date.strftime("%Y-%m-%d"))
+
+
+# compare version with find new version
+def check_for_new_version():
+    if ss.value('version'):
+        version = ss.value('version')
+    else:
+        version = None
+
+    # latest version
+    try:
+        latest_version = requests.get(f"{BASE_URL}/api/v1/videos/player-version").json()['version']
+        if not version:
+            ss.setValue("version", latest_version)
+            return True, latest_version
+        elif version != latest_version:
+            return False, "New version available"
+        else:
+            return True, latest_version
+    except requests.exceptions.ConnectionError:
+        return True, None
+
+
+def get_app_keys():
+    try:
+        url = f"{BASE_URL}/api/v1/videos/list-app-keys"
+        app_id = get_app_id()['id']
+        response = requests.get(url, params={'request_id': app_id})
+        if not response.status_code == 200:
+            return False, "An error occurred"
+    except requests.exceptions.ConnectionError:
+        return False, "No internet connection"
+    except Exception as e:
+        print(e)
+        return False, "An error occurred"
+
+    return True, response.json()['message']
+
+
+def sync_keys():
+    status, keys = get_app_keys()
+    if not status:
+        return False, keys
+
+    local_keys = get_local_keys()
+
+    # check if keys are registered on device
+    if len(keys) > 0:
+        new_keys = [
+            key for key in local_keys if key['key'] in keys
+        ]
+        Thread(target=register_keys, args=(new_keys,), daemon=False).start()
+
+
+# register keys to app
+def register_keys(keys):
+    if ss.value('keys'):
+        # append to existing keys
+        if not isinstance(keys, list):
+            existing_keys = ss.value('keys')
+            keys = [keys]
+            if existing_keys:
+                existing_keys.extend(keys)
+            else:
+                existing_keys = keys
+            ss.setValue('keys', existing_keys)
+        else:
+            ss.setValue('keys', keys)
+    else:
+        ss.setValue('keys', [keys])
+
+
+# get machine serial number
+def get_mac_serial_number():
+    output = subprocess.check_output(['system_profiler', 'SPHardwareDataType'])
+    lines = output.decode().split('\n')
+    for line in lines:
+        if 'Serial Number' in line:
+            return line.split()[-1]
+
+
+def get_windows_serial_number():
+    try:
+        output = subprocess.run(["wmic", "bios", "get", "serialnumber"], stdout=subprocess.PIPE).stdout.decode("utf-8")
+        serial_number = output.split("\n")[1].split(" ")[0]
+        return serial_number
+    except Exception as e:
+        print(e)
+        return None
+
+
+def get_serial_number():
+    os_type = platform.system()
+    if "darwin" in os_type.lower():
+        serial_number = get_mac_serial_number()
+    else:
+        serial_number = get_windows_serial_number()
+    return serial_number
+
+
+# get mac model name
+def get_mac_model_name():
+    pl = subprocess.run(["system_profiler", "SPHardwareDataType", "-xml"], capture_output=True).stdout
+    plist = plistlib.loads(pl)
+    model_name = plist[0]['_items'][0]['machine_model']
+    return model_name
+
+
+def get_windows_model_name():
+    output = subprocess.run("wmic csproduct get name", shell=True, capture_output=True, text=True)
+    try:
+        model_name = output.stdout.strip().split("\n")[2]
+    except IndexError:
+        model_name = output.stdout.strip().split("\n")[1]
+    return model_name
+
+
+# get machine model name
+def get_model_mame():
+    os_type = platform.system()
+    if 'darwin' in os_type.lower():
+        model_name = get_mac_model_name()
+    else:
+        model_name = get_windows_model_name()
+    return model_name
+
+
+# register app
+def register_app():
+    url = f"{BASE_URL}/api/v1/videos/app-registered"
+    payload = {"serial_number": get_serial_number(), "model_name": get_model_mame()}
+    response = requests.post(url, json=payload)
+
+    if not response.status_code == 200:
+        return False
+
+    try:
+        resp = response.json()['message']
+        conf = {'app': resp}
+        token = settings.TOKEN_KEY
+        key = base64.urlsafe_b64encode(hashlib.sha256(token.encode()).digest()[:32])
+        fernet = Fernet(key)
+        encrypted_data = fernet.encrypt(json.dumps(conf).encode())
+        ss.setValue('config', encrypted_data.decode())
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+# check if app's genuine
+def is_app_genuine():
+    if not ss.value('config'):
+        response = retrieve_headers()
+        if isinstance(response, dict):
+            delete_headers()
+            # register app
+            register_app()
+        return True
+    else:
+        # decrypt config
+        token = settings.TOKEN_KEY
+        key = base64.urlsafe_b64encode(hashlib.sha256(token.encode()).digest()[:32])
+        fernet = Fernet(key)
+        try:
+            encrypted_data = ss.value("config")
+            decrypted_data = fernet.decrypt(encrypted_data.encode())
+            return json.loads(decrypted_data)
+        except Exception as e:
+            print(e)
+            return False
+
+
+# get app's id
+def get_app_id():
+    config = is_app_genuine()
+    if isinstance(config, dict):
+        return config['app']
+    return False
+
+
+# get alternate registered keys on other devices
+def get_registered_keys():
+    try:
+        url = f"{BASE_URL}/api/v1/videos/list-client-keys"
+        headers = retrieve_headers()
+        app_id = get_app_id()['id']
+        headers.update({'request_id': app_id})
+        response = requests.post(url, json=headers)
+        if not response.status_code == 200:
+            return False, "An error occurred"
+
+        return True, response.json()['message']
+    except requests.exceptions.ConnectionError:
+        return False, "No internet connection"
